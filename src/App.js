48cf9d03659d8d3fb3528e5034b798a589144610
Neelima1220 <neelimareddy1220@gmail.com>
2022-06-03 07:33:28 +0300

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,58 +1,86 @@
 import React, { useEffect, useState } from 'react';
+// import { debounce } from 'loadash';
 import './style.css';
 
 export default function App() {
   const [data, setData] = useState([]);
   const [post, setPost] = useState('');
+  const [resfound, setResfound] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    const getData = async () => {
-      const response = await fetch(
-        'https://jsonplaceholder.typicode.com/todos'
-      );
-      const data = await response.json();
-      setData(data.slice(0, 5));
-    };
-    getData();
-  }, []);
+  // useEffect(() => {
+  // const getData = async () => {
+  //   setIsLoading(true);
+  //   const response = await fetch('https://restcountries.com/v3.1/all');
+  //   const data = await response.json();
+  //   console.log(data);
+  //   setData(data);
+  //   setIsLoading(false);
+  // };
 
-  const handleAdd = (e) => {
-    e.preventDefault();
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-      method: 'POST',
-      body: JSON.stringify({
-        completed: false,
-        id: data.length,
-        title: post,
-        userId: new Date().getTime(),
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
-    })
-      .then((response) => response.json())
-      .then((res) => console.log(res));
+  const handleChange = async (e) => {
+    setPost(e.target.value);
+    setIsLoading(true);
+    const response = await fetch('https://restcountries.com/v3.1/all');
+    const data = await response.json();
+    setData(data);
+    setIsLoading(false);
   };
-
   return (
-    <div>
-      <h1>Hello autoComplete!</h1>
-      <input value={post} onChange={(e) => setPost(e.target.value)} />
-      {data
-        .filter((item) => {
-          if (post === '') {
-            return item;
-          }
-          return item.title.toLowerCase().includes(post.toLowerCase());
-        })
-        .map((item) => {
-          return (
-            <p>
-              {item.id}. {item.title}
-            </p>
-          );
-        })}
-      <button onClick={handleAdd}>Submit</button>
+    <div className="main-div">
+      <input value={post} onChange={handleChange} className="input" />
+      {!isLoading ? (
+        <div className="suggest-countries">
+          <ul
+            style={{
+              listStyleType: 'none',
+              paddingLeft: '0px',
+            }}
+          >
+            {data
+              .filter((item) => {
+                if (post === '') {
+                  return item;
+                }
+                if (
+                  item.name.official.toLowerCase().includes(post.toLowerCase())
+                ) {
+                  return <p>'not found'</p>;
+                }
+                return item.name.official
+                  .toLowerCase()
+                  .includes(post.toLowerCase());
+              })
+              .map((item) => {
+                return <li>{item.name.official} </li>;
+              })}
+          </ul>
+        </div>
+      ) : (
+        <p>Loading</p>
+      )}
     </div>
   );
 }
+
+{
+  /* <button onClick={handleAdd}>Submit</button> */
+}
+
+// const handleAdd = (e) => {
+//   e.preventDefault();
+//   fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//       completed: false,
+//       id: data.length,
+//       title: post,
+//       userId: new Date().getTime(),
+//     }),
+//     headers: {
+//       'Content-type': 'application/json; charset=UTF-8',
+//     },
+//   })
+//     .then((response) => response.json())
+//     .then((res) => console.log(res));
+// };
